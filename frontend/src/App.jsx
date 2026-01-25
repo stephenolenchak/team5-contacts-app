d4bb22cde9609982dbb3dc8a81500481edd14c8e
Stephen Olenchak <stephenolenchak@gmail.com>
2026-01-25 18:58:18 -0500

    Add grow transition

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { Route } from 'react-router-dom';
 import { IndexPage } from "./pages/IndexPage";
 import Box from '@mui/material/Box';
 import Footer from './components/Footer';
+import { Grow } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,13 +21,15 @@ function App() {
         <CssBaseline />
         <Navbar />
         <Toolbar />
+        <Grow in timeout={400} style={{ transformOrigin: '0 0 0' }}>
         <Box  sx={{textAlign: 'center', backgroundColor: "#0f243a", width: "95vw", height: "90vh", margin: "0 auto", borderRadius: 10, mt: 4,display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column",}}>
         <Box sx={{ height: 24 }} />
-        <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
         </Box>
+         </Grow>
       </ThemeProvider>
       <Toolbar />
       <Footer />
