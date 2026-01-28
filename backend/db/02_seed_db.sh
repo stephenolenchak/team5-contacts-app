8fce9f7b45457135b83b5c8be04253827db6a011
Stephen Olenchak <stephenolenchak@gmail.com>
2026-01-27 23:53:15 -0500

    Updates

--- backend/db/02_seed_db.sh
+++ backend/db/02_seed_db.sh
@@ -3,7 +3,7 @@
 # Seed the database
 if [ "$SEED_DB" = "true" ]; then
     echo "Seeding database..."
-    mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASS < ../seed.sql
+    mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASS < 03_seed.sql
     echo "Database seeded!"
 else
     echo "Database is not seeded"
